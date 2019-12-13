91978b2aaf9f2c1de042ff90e9eb63ee03b0d761
Diego Flores <diegoflores@dhcp-10-250-38-162.harvard.edu>
2019-12-13 18:51:50 -0500

    I have finished the project, the video will be coming shortly as well as the pdf

--- democracy-effectiveness/app.R
+++ democracy-effectiveness/app.R
@@ -34,8 +34,8 @@ ui <- fluidPage(theme = shinytheme("superhero"),
                                                 size = 1),
                                     plotOutput("demGrowth"),
                                     h3("Analysis"),
-                                    p("The general trends of these lines suggest that more and dore counties are opting in to switch to democracies. The number of autocracies in the world is
-                                      clearly diminishing and it appears as if those countries leaving the autocratic forms of government behind are the ones adeopting democracy. ")
+                                    p("The general trends of these lines suggest that more and more countries are opting in to switch to democratic regime types. The number of autocracies in the world is
+                                      clearly diminishing and it appears as if those countries leaving the autocratic forms of government behind are the ones adopting democracy. ")
                            ),
                            
                            tabPanel("About",
@@ -44,21 +44,22 @@ ui <- fluidPage(theme = shinytheme("superhero"),
                                     # data is
                                     
                                     h1("The Project"),
-                                    p("The number of democracies in the world has skyrocketed within the oast centuries. Within the United
+                                    p("The number of democracies in the world has skyrocketed within the past century. Within the United States
                           there is a general notion that anything done within the United States is the best and unimprovable.  
                           This feeling extends to the form of government running the country. So, is Democracy truly the best form
-                          of government? How does one test that? In this project I aimto determine if data can help support this claim 
+                          of government? How does one test that? In this project I aim to determine if data can help support this claim. 
                           As a result, I sought to analyze the effect that a change to a democratic government would have on a country in 
                           terms of factors that I could find data for. These came in variables such as 'Happiness', 'Life Expectancy', 
                           'Population Density', and 'Population' among others. Through viewing the relationships between these sets of 
-                          data and the growth of democracy, I hoped to show that democracy has proof to claim that it is a 
+                          data and the growth of democracy, I hoped to show whether democracy has proof or not to claim that it is a 
                           good form of government."),
                                     h2("The Data"),
                                     p("These visualizations are based on the data sets: 'Democracy' put together by Max
                           Roser in June 2019 'Happiness', put together by Esteban Ortiz and Max Roser, first published in 2013 
                           but revised in 2017 'Life Expectancy', put together by Max Roser in 2015 and 'World Population Growth',
                           put together by Max Roser, Hannah Ritchie and Esteban Ortiz-Ospina in 2019. These data sets can be found
-                          in the cata center https://ourworldindata.org"),
+                          in the data center https://ourworldindata.org. The World Bank was also a valuable asset in terms of finding
+                          data on GDP and regime type which was used to draw further analysis."),
                                     h3("About Me"),
                                     p("My name is Diego Flores. I am a first-year from the Bronx, New York on track to concentrate in 
                           Government although that is subject to change."),
@@ -114,7 +115,8 @@ ui <- fluidPage(theme = shinytheme("superhero"),
                                     plotOutput("dem_le"),
                                     h3("Analysis"),
                                     p("The overall life expectancy for all of these countries appears to have already been on an upward trend
-                                      to begin with before the introduction of democracy. A relationship here seems a bit farfetched.")
+                                      to begin with before the introduction of democracy. A relationship here seems a bit farfetched as the data suggests
+                                      that life expectancy is a factor that increases generally as time goes by.")
                            ), 
                            
                            tabPanel("Human Development Index (HDI)",
@@ -184,14 +186,80 @@ ui <- fluidPage(theme = shinytheme("superhero"),
                                   h3("Analysis"),
                                   p("The overall happiness rating for all of these countries appears to be dependant on more than the factor of 
                                       type of government. As a result, finding a direct relationship with the introduction of democracy and overall 
-                                      happiness seems difficult and unlikely here.")
+                                      happiness seems difficult and unlikely within these specific graphs. It appears as if for specific countries that did experience 
+                                    a direct transition to democracy there is no explicit effect from democratization.")
                          ),
                          
                          tabPanel("Analysis of Variables",
-                                  p("Choose a variable to see a regression with all the data points available for that variable."))           
+                                  titlePanel("Regression Analysis"),
+                                  
+                                  # Sidebar layout with input and output definitions ----
+                                  sidebarLayout(
                                     
+                                    # Sidebar panel for inputs ----
+                                    sidebarPanel(
+                                      
+                                      # Input: Select the random distribution type ----
+                                      
+                                      
+                                      # Input: Slider for the number of observations to generate ----
+                                      sliderInput("year",
+                                                  "Year for Observations:",
+                                                  value = 2010,
+                                                  min = 2000,
+                                                  max = 2017,
+                                                  sep = "")
+                                      
+                                    ),
                                     
-                                    #So here I think I will 
+                                    # Main panel for displaying outputs ----
+                                    mainPanel(
+                                      
+                                      # Output: Tabset w/ plot, summary, and table ----
+                                      tabsetPanel(type = "tabs",
+                                                  tabPanel("GDP", 
+                                                           plotOutput("plot7"),
+                                                           p("From these graphs it can be seen from the regressions that countries which are democratic tend to have higher GDP per Capitas
+                                                             than countries that are not democratic, this appears to be true throughout all years. This finding supports the suggestion from 
+                                                             the GDP and Democracy tab where we were inclined to suggest that democracy may aide a country in bettering its economic situation")),
+                                                  tabPanel("Life Expectancy", plotOutput("plot8"),
+                                                           p("The regressions support the assumption I had made before doing any data analysis although the Life Expectancy tab appears to go against it.
+                                                             It would be easy to suggest that democracy leads to higher life expectancy, but our Life Expectancy tab, which analyzes specidic countries
+                                                             appears to suggests that these countries were already experiencing increases in Life Expectancy without the presence of democracy. These
+                                                             regressions however, alllow us to take a step back and see that overall, in Democratic countries have higher life expectancies than countries
+                                                             that are not democratic. This is a similar finding to the other three of our variables we analyzed.")),
+                                                  tabPanel("HDI", plotOutput("plot9"),
+                                                           p("These regressions appear to tell a similar story to the ones from Life Expectancy. While perhaps the introduction of democracy
+                                                             may not have affected the already increasing rate of HDI in specific countries, by taking a broader scope through regressions which
+                                                             compare Democratic Countries to those that are not, within specific years, it is easy to see that democratic countries have significantly
+                                                             higher scored for HDI. This suggests that there may in fact be a relationship between the two: Democracies tend to have higher HDI")),
+                                                  tabPanel("Happiness", plotOutput("plot10"),
+                                                           p("Once again these regressions suggest that Democratic Countries appear to be happier than non-democratic countries. While
+                                                             the other tab in this project anaylyzing happiness within specific countries before and after democracy was implemented, 
+                                                             appeared to find no real relationship, this different apprach does.")),
+                                                  tabPanel("Infant Mortality", plotOutput("plot11"),
+                                                           p("It appears as if infant mortality rate, within this selected time window, will be low despite the political structure of
+                                                             a country. It supports our assumption we made in the IM tab that the IM rate decreases by virtue of the advancement of human
+                                                             society as a whole, and should not be attributed to any political system. This is great, to know that IM rate has always been on 
+                                                             a general downward trend")),
+                                                  tabPanel("Conclusions",
+                                                           p("It is pretty clear from our regressions that in the larger picture, the presence of democracy does have an effect
+                                                             on variables that we attribute to the prosperity of a society. However, our specific analyses of singular countries
+                                                             before and after they became democratic lead me to believe that the adoption of democracy itself is not the sole
+                                                             factor at play that affects these variables. Many of the countries that are not democratic simply never have been nor have they tried 
+                                                             to be democratic. As a result it can be suggested that perhaps the countries we analysed already had systems in place that allowed
+                                                             democracy to be implemented. Perhaps these bettered conditions reflected in the regressions were not because of democracy, but maybe they were
+                                                             the conditions necessary for democracy to grow. This suggestsion echoes theories such as that of Lipset's theory of modernization where he
+                                                             claims that economic development is the breeding ground for democracy along with improved conditions of survival. That being said however,
+                                                             it must be acknowledged that democratic countries, based off of this analysis have overall better conditions of living, thus making them
+                                                             better and easier places to live."))
+                                      )
+                                      
+                                    )
+                                  )
+                         ) 
+                                    
+                                   
                                     
                                     ))
                 
@@ -714,6 +782,60 @@ server <- function(input, output) {
     geom_vline(xintercept = 2006, color = "blue")
   
 }
-})
-  }
+  })
+  
+  output$plot7 <- renderPlot({
+    year <- input$year
+    
+    full %>% filter(year == year) %>% 
+      ggplot(aes(x = democracy, y = value)) + geom_point() + 
+      geom_jitter(height = 0.2, width = 0.2) +
+      geom_smooth(method = "lm", se = FALSE)  + labs(y = "Value", x = "Non-Democratic (0) or Democratic (1)") +
+      ggtitle("GDP per Capita of Democratic vs Non-Democratic Countries")
+    
+    
+
+  })
+  
+  # Generate a summary of the data ----
+  output$plot8 <- renderPlot({
+    year <- input$year
+    full %>% filter(year == year) %>% 
+      ggplot(aes(x = democracy, y = expectancy)) + geom_point() + 
+      geom_jitter(height = 0.2, width = 0.2) +
+      geom_smooth(method = "lm", se = FALSE)  + 
+      labs(y = "Life Expectancy", x = "Non-Democratic (0) or Democratic (1)") +
+      ggtitle("Life Expectancy of Democratic vs Non-Democratic Countries")
+    
+  })
+  
+  # Generate an HTML table view of the data ----
+  output$plot9 <- renderPlot({
+    year <- input$year
+    full %>% filter(year == year) %>% 
+      ggplot(aes(x = democracy, y = hdi)) + geom_point() + 
+      geom_jitter(height = 0.2, width = 0.2) +
+      geom_smooth(method = "lm", se = FALSE)  + labs(y = "Value", x = "Non-Democratic (0) or Democratic (1)") +
+      ggtitle("HDI of Democratic vs Non-Democratic Countries")
+  })
+  
+  output$plot10 <- renderPlot({
+    year <- input$year
+    full %>% filter(year == year) %>% 
+      ggplot(aes(x = democracy, y = cantril_score)) + geom_point() + 
+      geom_jitter(height = 0.2, width = 0.2) +
+      geom_smooth(method = "lm", se = FALSE)  + labs(y = "Value", x = "Non-Democratic (0) or Democratic (1)")+
+      ggtitle("Happiness of Democratic vs Non-Democratic Countries")
+  })
+  output$plot11 <- renderPlot({
+    year <- input$year
+    full %>% filter(year == year) %>% 
+      ggplot(aes(x = democracy, y = rate)) + geom_point() + 
+      geom_jitter(height = 0.2, width = 0.2) +
+      geom_smooth(method = "lm", se = FALSE)  + labs(y = "Value", x = "Non-Democratic (0) or Democratic (1)") +
+      ggtitle("Infant Mortality Rates of Democratic vs Non-Democratic Countries")
+
+  })
+}
+  
 shinyApp(ui = ui, server = server)
